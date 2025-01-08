d7e9cab2e920431b8773142136f61d9edfccec7e
torofms37 <torofms37@gmail.com>
2025-01-07 22:33:37 -0700

    cosas

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 const {response} = require('express');
+const bcrypt = require('bcryptjs');
 const { validationResult } = require('express-validator');
 
 const Usuario = require('../models/Usuario')
@@ -15,6 +16,11 @@ const createUser = async(req, res = response) => {
       })
     }
       usuario = new Usuario(req.body);
+
+      //Encriptar pass
+      const salt = bcrypt.genSaltSync();
+      usuario.password = bcrypt.hashSync(password, salt)
+
       await usuario.save();
       res.status(201).json({
         ok: true,
@@ -32,15 +38,41 @@ const createUser = async(req, res = response) => {
 
 //
 
-const loginUser = (req, res = response) => {
+const loginUser = async(req, res = response) => {
   const {email, password} = req.body;  
-  
-  res.status(201).json({
-    ok: true,
-    msg: 'login',
-    email,
-    password,
-  });
+
+  try {
+    const usuario = await Usuario.findOne({email});
+    if (!usuario) {
+      return res.status(400).json({
+        ok: false,
+        msg: 'El usuario ya existe we, con ese email'
+      })
+    };
+
+    //Confirmas los pass
+    const validPassword = bcrypt.compareSync(password, usuario.password);
+    if (!validPassword) {
+      return res.status(400).json({
+        ok: false,
+        msg: 'Password incorrecto'
+      })
+    };
+
+    //Generar uestor JWT
+    res.json({
+      ok: true,
+      uid: usuario.id,
+      name: usuario.name
+    })
+
+  } catch (error) {
+    res.status(500).json({
+      ok: false,
+      msg: 'por favor hable con el admin'
+    })
+  }
+
 }
 
 //
