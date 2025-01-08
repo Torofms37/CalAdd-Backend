9c7efb5e335076f4f82f8e850bf2f7425ddf2c3d
torofms37 <torofms37@gmail.com>
2025-01-07 18:36:27 -0700

    cosas nuevass

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,33 @@
 const {response} = require('express');
 const { validationResult } = require('express-validator');
 
-const createUser = (req, res = response) => {
-  const {name, email, password} = req.body;
-  
-  res.status(201).json({
-    ok: true,
-    msg: "register",
-    name,
-    email,
-    password
-  });
+const Usuario = require('../models/Usuario')
+
+const createUser = async(req, res = response) => {
+  const {email, password} = req.body;
+
+  try {
+    let usuario = await Usuario.findOne({email});
+    if (usuario) {
+      return res.status(400).json({
+        ok: false,
+        msg: 'El usuario ya existe we'
+      })
+    }
+      usuario = new Usuario(req.body);
+      await usuario.save();
+      res.status(201).json({
+        ok: true,
+        uid: usuario.id,
+        name: usuario.name
+      });
+    
+  } catch (error) {
+    res.status(500).json({
+      ok: false,
+      msg: 'por favor hable con el admin'
+    })
+  }
 }
 
 //
